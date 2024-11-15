82cf121a61654b62e69900ce0bf7ede8e75a8465
Chris Martin <shady@shadyproject.net>
2024-11-14 21:39:28 -0800

    spanish and french

--- hello_test.go
+++ hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Madeline", "French")
+		want := "Bonjour, Madeline"
 		assertCorrectMessage(t, got, want)
 	})
 }
